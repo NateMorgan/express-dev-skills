7348f24f28452a7852641e9ba87aef31f1893921
Nate Morgan <nathanielrmorgan@gmail.com>
2022-09-20 16:42:29 -0400

    add a skills datafile

---
+++ data/skills.js
@@ -0,0 +1,6 @@
+skills = [
+  { skillName: "Coding", type: "Professional", mastery: "Novice"},
+  { skillName: "Soccer", type: "Sport", mastery: "Advanced"},
+  { skillName: "Climbing", type: "Sport", mastery: "Novice"},
+  { skillName: "Juggling", type: "Hobby", mastery: "Intermediate"}
+]
