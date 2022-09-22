cd67a04104309060928d2a526e2ecacbcc476b6e
Nate Morgan <nathanielrmorgan@gmail.com>
2022-09-22 19:16:14 -0400

    clean up lab

--- controllers/skills.js
+++ controllers/skills.js
@@ -3,7 +3,7 @@ import {Skill} from '../models/skill.js'
 function index(req, res) {
   Skill.find({})
   .then(skills => {
-    res.render('skills', {
+    res.render('skills/index', {
       skills:skills
     })
   })
