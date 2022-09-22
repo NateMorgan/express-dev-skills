1d93b7339516e3a19f125cdbb281545c7313cf37
Nate Morgan <nathanielrmorgan@gmail.com>
2022-09-21 20:58:18 -0400

    added individual skill view

--- routes/skills.js
+++ routes/skills.js
@@ -6,7 +6,9 @@ const router = Router()
 /* GET users listing. */
 router.get('/', skillsCtr.index)
 router.get('/new', skillsCtr.new)
+router.get('/:id', skillsCtr.show)
 router.post('/', skillsCtr.create)
+
 
 export {
   router
